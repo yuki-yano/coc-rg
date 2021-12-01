35cf5f9b4b0e4eae741dd1b6bf99a4d8ba4614a3
yuki_yano <yuki.ycino@gmail.com>
2021-12-01 15:30:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# coc-rg
+
+## Install
+
+`:CocInstall coc-rg`
+
+## License
+
+MIT
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { CompleteResult, ExtensionContext, sources } from 'coc.nvim';
+import { execSync } from 'child_process';
+
+export const activate = async (context: ExtensionContext): Promise<void> => {
+  context.subscriptions.push(
+    sources.createSource({
+      name: 'rg',
+      doComplete: async ({ input }) => {
+        return await getCompletionItems({ input: input.replace(/([\\\[\]^$.*])/g, '\\$1') });
+      },
+    })
+  );
+};
+
+const options = [
+  '--no-filename',
+  '--no-heading',
+  '--no-line-number',
+  '--color=never',
+  '--only-matching',
+  '--word-regexp',
+  '--ignore-case',
+];
+
+const getCompletionItems = async ({ input }: { input: string }): Promise<CompleteResult> => {
+  if (input.length < 2) {
+    return { items: [] };
+  }
+
+  const stdout: string = execSync(`rg ${options.join(' ')} ${input}[a-zA-Z0-9_-]+ ${process.cwd()}`, {
+    encoding: 'utf-8',
+    maxBuffer: 1024 * 1024 * 1000,
+  });
+
+  return {
+    items: stdout.split('\n').map((word) => ({ word, menu: '[rg]' })),
+  };
+};
